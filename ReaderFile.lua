de52d6294816465b556a2be94e49baafefcc2761
Zunzz <155456585+Zunzz@users.noreply.github.com>
2024-01-02 17:00:11 -0300

    ReaderFile.lua adicionado ao repositório.

---
+++ ReaderFile.lua
@@ -0,0 +1,52 @@
+local ReaderFile = {}
+ReaderFile.mFile = nil
+ReaderFile.mBytesHandler = nil
+
+function ReaderFile:new(filename, mode)
+    local object = {}
+    setmetatable(object, self)
+    self.__index = self
+    self.mFile = io.open(filename, mode)
+    return object
+end
+
+function ReaderFile:setBytesHandler(BytesHandler)
+    self.mBytesHandler = BytesHandler
+end
+
+function ReaderFile:readBufferFromOffset(offset, length)
+    local currentPosition = self.mFile:seek("cur")
+    self.mFile:seek("set", offset)
+    local buffer = self.mFile:read(length)
+    self.mFile:seek("set", currentPosition)
+    return buffer
+end
+
+function ReaderFile:readByteFromOffset(offset)
+    local buffer = self:readBufferFromOffset(offset, 1)
+    local byte = buffer:byte(1, 1)
+    return byte
+end
+
+function ReaderFile:readInteger16FromOffset(offset)
+    local buffer = self:readBufferFromOffset(offset, 2)
+    local bytes = {buffer:byte(1, -1)}
+    return self.mBytesHandler:toInteger(bytes)
+end
+
+function ReaderFile:readInteger32FromOffset(offset)
+    local buffer = self:readBufferFromOffset(offset, 4)
+    return self.mBytesHandler:toInteger(bytes)
+end
+
+function ReaderFile:readInteger64FromOffset(offset)
+    local buffer = self:readBufferFromOffset(offset, 8)
+    local bytes = {buffer:byte(1, -1)}
+    return self.mBytesHandler:toInteger(bytes)
+end
+
+function ReaderFile:close()
+    self.mFile:close()
+end
+
+return ReaderFile
